e9a4585a46b1d3aa3b9b8fcc060a1b6b38c5958f
itzRaellK <israel-kessler@hotmail.com>
2022-02-21 05:57:43 -0300

    Estudos 21/02/2022

---
+++ Linguagem de Programação Aplicada/Aula 01/exerciciosAula01.py
@@ -0,0 +1,20 @@
+## Meus exercícios de python
+## Bora programar!
+
+def soma(a,b):
+    add = a + b
+    return add
+
+x = soma(55, -5)
+print(x)
+
+a = 5
+if a < 5:
+    print('Menor que 5')
+elif a >= 5:
+    print('Maior ou igual a 5')
+else:
+    print('Diferente de 5')
+
+texto = "Análise de Dados"
+print(texto[11:])
